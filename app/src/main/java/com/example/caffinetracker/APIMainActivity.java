d3da2e344ece33fb0dd9f7cecb3f027e70b10797
Justin Morales <magagod2@gmail.com>
2019-05-13 00:10:00 -0700

    added some API stuff, still not working

--- app/src/main/java/com/example/caffinetracker/APIMainActivity.java
+++ app/src/main/java/com/example/caffinetracker/APIMainActivity.java
@@ -2,12 +2,114 @@ package com.example.caffinetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import com.example.caffinetracker.model.FoodItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class APIMainActivity extends AppCompatActivity {
+
+    private static final int SINGLE_ITEM_REQUEST = 100;
+
+    public static final String RETURN_QUANTITY = "RETURN_QUANTITY";
+
+    // out arguments (return)
+    public static final String RETURN_FOOD_NAME = "RETURN_FOOD_NAME";
+    public static final String RETURN_FOOD_CATEGORY = "RETURN_FOOD_CATEGORY";
+    public static final String RETURN_FOOD_NDB = "RETURN_FOOD_NDB";
+    public static final String RETURN_FOOD_MEASURE = "RETURN_FOOD_MEASURE";
+    public static final String RETURN_FOOD_QUANTITY = "RETURN_FOOD_QUANTITY";
+
+    private final String TAG_SEARCH_MEASURE = "USDAQuery-SearchMeasure";
+    private final String TAG_SEARCH_NAME = "USDAQuery-SearchName";
+
+    private List<FoodItem> foodItems;
+    private RequestQueue requestQueue;
+    private int chosenItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apimain);
+
+        foodItems = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest()
+
     }
+
+    private StringRequest searchNameStringRequest(String nameSearch) {
+        final String API = "&api_key=spkVUDxuyvhsVBJTwpF53KaGNaytyLFPQRVWZePq";
+        final String NAME_SEARCH = "&q=";
+        final String DATA_SOURCE = "&ds=Standard Reference";
+        final String FOOD_GROUP = "&fg=";
+        final String SORT = "&sort=r";
+        final String MAX_ROWS = "&max=25";
+        final String BEGINNING_ROW = "&offset=0";
+        final String URL_PREFIX = "https://api.nal.usda.gov/ndb/search/?format=json";
+
+        String url = URL_PREFIX + API + NAME_SEARCH + nameSearch + DATA_SOURCE + FOOD_GROUP + SORT + MAX_ROWS + BEGINNING_ROW;
+
+        // 1st param => type of method (GET/PUT/POST/PATCH/etc)
+        // 2nd param => complete url of the API
+        // 3rd param => Response.Listener -> Success procedure
+        // 4th param => Response.ErrorListener -> Error procedure
+        return new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    // 3rd param - method onResponse lays the code procedure of success return
+                    // SUCCESS
+                    @Override
+                    public void onResponse(String response) {
+                        // try/catch block for returned JSON data
+                        // see API's documentation for returned format
+                        try {
+                            JSONObject result = new JSONObject(response).getJSONObject("list");
+                            int maxItems = result.getInt("end");
+                            JSONArray resultList = result.getJSONArray("item");
+
+                            for (int i = 0; i < maxItems; i++) {
+                                FoodItem fi = new FoodItem(
+                                        resultList.getJSONObject(i).getString("name").trim(),
+                                        resultList.getJSONObject(i).getString("group").trim(),
+                                        resultList.getJSONObject(i).getString("ndbno").trim(),
+                                        ""
+                                );
+                                foodItems.add(fi);
+                            }
+
+                            //This call show call a function to an adapter. Create that function in this class.
+                            showListOfItems();
+
+                            // catch for the JSON parsing error
+                        } catch (JSONException e) {
+                            Toast.makeText(APIMainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    } // public void onResponse(String response)
+                }, // Response.Listener<String>()
+                new Response.ErrorListener() {
+                    // 4th param - method onErrorResponse lays the code procedure of error return
+                    // ERROR
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // display a simple message on the screen
+                        Toast.makeText(APIMainActivity.this, "Food source is not responding (USDA API)", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
 }
